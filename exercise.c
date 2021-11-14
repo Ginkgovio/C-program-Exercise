373ae47850993b3d7348bf60adfbd807168647a4
Aless0a <2673263825@qq.com>
2021-11-14 10:41:34 +0800

    2021 11.14 练习1

---
+++ exercise.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <windows.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	char arr1[] = { "welcome to haven!" };
+	char arr2[] = { "#################" };
+	int left, right;
+	left = 0;
+	right = strlen(arr1) - 1;
+
+	while (left <= right)
+	{
+		arr2[left] = arr1[left];
+		arr2[right] = arr1[right];
+		printf("%s\n", arr2);
+		Sleep(1000);
+		system("cls");
+		left++;
+		right--;
+	}
+	printf("%s\n", arr2);
+	return 0;
+}
